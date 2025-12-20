6814a8229d1246f8fe9b698b8eb123bb86c3eb1c
Utkarsh1-ux <utkarshramu53@gmail.com>
2025-12-20 16:14:22 +0530

    Completed collection with search bar and all

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import{ products } from "../assets/assets.js"
+import { useSearchParams } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -7,10 +8,12 @@ const ShopContextProvider = (props)=>{
 
     const currency = "₹";
     const delivery_fee = 10;
-    
+    const [search , setSearch] = useState('')
+    const [showSearch , setShowSearch] = useState(false)
 
        const value = {
-         products, currency,delivery_fee
+         products, currency,delivery_fee,search,
+         setSearch,showSearch,setShowSearch
        }
        return(
         <ShopContext.Provider value={value}>
